e89d38b7d9cb625483cd4b28bcb82a9a6639b66d
Asaf Ahmed <asaf1582003@gmail.com>
2025-10-31 14:58:36 +0530

    component

---
+++ ES6/app.js
@@ -0,0 +1,3 @@
+import * as math from 'C:/Users/asaf1/OneDrive/Desktop/ABES DS(B)/ES6/math.js';
+console.log(math.add(5, 3));
+console.log(math.subtract(5, 3));
---
+++ asaf/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Card.css'
+function Card() {
+  return (
+    <div id ="card">
+        <img src="" alt="" height={100} width = {100} />
+        <h1>ABES EC</h1>
+        <h1>B.Tech</h1>
+    </div>
+  )
+}
+
+export default Card
